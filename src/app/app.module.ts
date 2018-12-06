d9a91a98abdc7844a5d03ca5a71f019b6846898c
Simon <nomisvank@hotmail.com>
2018-12-06 15:03:18 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,46 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContactenComponent } from './contacten/contacten.component';
+import { OnkostenComponent } from './onkosten/onkosten.component';
+import { BelastingComponent } from './belasting/belasting.component';
+import { InstellingenComponent } from './instellingen/instellingen.component';
+import { BriefTikkenComponent } from './contacten/brief-tikken/brief-tikken.component';
+import { OfferteTikkenComponent } from './contacten/offerte-tikken/offerte-tikken.component';
+import { FactuurTikkenComponent } from './contacten/factuur-tikken/factuur-tikken.component';
+import { ContactToevoegenComponent } from './contacten/contact-toevoegen/contact-toevoegen.component';
+import { ContactWijzigenComponent } from './contacten/contact-wijzigen/contact-wijzigen.component';
+import { ContactItemComponent } from './contacten/contact-item/contact-item.component';
+import { OnkostenToevoegenComponent } from './onkosten/onkosten-toevoegen/onkosten-toevoegen.component';
+import { OnkostenItemComponent } from './onkosten/onkosten-item/onkosten-item.component';
+import { BelastingListComponent } from './belasting/belasting-list/belasting-list.component';
+import { BelastingItemComponent } from './belasting/belasting-list/belasting-item/belasting-item.component';
+import { KostenpostListComponent } from './instellingen/kostenpost-list/kostenpost-list.component';
+import { KostenpostItemComponent } from './instellingen/kostenpost-list/kostenpost-item/kostenpost-item.component';
+import { BtwWijzigenComponent } from './instellingen/btw-wijzigen/btw-wijzigen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContactenComponent,
+    OnkostenComponent,
+    BelastingComponent,
+    InstellingenComponent,
+    BriefTikkenComponent,
+    OfferteTikkenComponent,
+    FactuurTikkenComponent,
+    ContactToevoegenComponent,
+    ContactWijzigenComponent,
+    ContactItemComponent,
+    OnkostenToevoegenComponent,
+    OnkostenItemComponent,
+    BelastingListComponent,
+    BelastingItemComponent,
+    KostenpostListComponent,
+    KostenpostItemComponent,
+    BtwWijzigenComponent
   ],
   imports: [
     BrowserModule
