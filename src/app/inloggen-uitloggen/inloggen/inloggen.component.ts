149cd7b3673e0a9eb77121dc5c75c5deaa25d071
Jacco van den Berg <jaccoberg2281@gmail.com>
2018-12-10 12:07:13 +0100

    Login scherm front end gemaakt

--- src/app/inloggen-uitloggen/inloggen/inloggen.component.ts
+++ src/app/inloggen-uitloggen/inloggen/inloggen.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-inloggen',
   templateUrl: './inloggen.component.html',
-  styleUrls: ['./inloggen.component.css']
+  styleUrls: ['./inloggen.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class InloggenComponent implements OnInit {
 
