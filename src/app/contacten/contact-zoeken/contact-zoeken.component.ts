9d30c4fb74f201fc8a4d4504dc2dc2039e87823e
Unknown <s1107083@student.hsleiden.nl>
2018-12-12 15:18:52 +0100

    Werkt maar nog met hardcoded info, moet nog verbinding met backend komen

--- src/app/contacten/contact-zoeken/contact-zoeken.component.ts
+++ src/app/contacten/contact-zoeken/contact-zoeken.component.ts
@@ -7,22 +7,45 @@ import {Contact} from '../contact.model';
   styleUrls: ['./contact-zoeken.component.css']
 })
 export class ContactZoekenComponent implements OnInit {
-  @Output('bedrijfGezocht') gekozenContact = new EventEmitter<Contact>();
+  @Output() bedrijfGezocht = new EventEmitter<Contact>();
   @ViewChild('bedrijfInput') bedrijfNaam: ElementRef;
+  mogelijkeBedrijven: {id: number, naam: string}[];
 
   constructor() { }
 
 
   ngOnInit() {
+    this.krijgMogelijkeBedrijven();
   }
 
   onZoekBedrijf() {
     const naam = this.bedrijfNaam.nativeElement.value;
+    let naamIndex = null; // = this.mogelijkeBedrijven.indexOf(naam);
+
+    for (const bedrijf of this.mogelijkeBedrijven) {
+      if (bedrijf.naam === naam) {
+        naamIndex = this.mogelijkeBedrijven.indexOf(bedrijf);
+      }
+    }
+    if (naamIndex === null) {
+      // TODO foutmelding geven
+      console.log('dit bedrijf bestaat niet')
+      return;
+    }
+    const id = this.mogelijkeBedrijven[naamIndex].id;
+    console.log(id);
+
     // TODO uit database zoeken
     const contact = new Contact(1, 'voor', 'achter', 'bedrijf',
       'straat', 'postcode', 'woonplaats', 'nederland', [12345678, 87654321, 34567890],
       ['iemand@iets.wat', 'haha@fheod.nl'], 'relatie', 'website.nl');
-    this.gekozenContact.emit(contact);
+
+    this.bedrijfGezocht.emit(contact);
+  }
+
+  krijgMogelijkeBedrijven() {
+    // TODO uit de backhand alle bedrijfnamen + id halen
+    this.mogelijkeBedrijven = [{id: 0, naam: 'Abedrijf'}, {id: 1, naam: 'Bbedrijf'}, {id: 2, naam: 'Cbedrijf'}];
   }
 
 }
