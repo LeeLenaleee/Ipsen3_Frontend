a62dc970a851a6bde631cbe28f2d03fdd489c523
Jacco van den Berg <jaccoberg2281@gmail.com>
2018-12-13 15:18:14 +0100

    fake databse credentials kunnen gechedcked worden en krijg je juiste melding

--- src/app/inloggen-uitloggen/inloggen/inloggen.component.ts
+++ src/app/inloggen-uitloggen/inloggen/inloggen.component.ts
@@ -22,9 +22,29 @@ export class InloggenComponent implements OnInit {
   inloggen() {
     const email = this.emailAdress.nativeElement.value;
     const wachtwoord = this.password.nativeElement.value;
-    this.apiUrl3 = 'http://localhost:8080/api?name=' + wachtwoord;
-    const response = this.http.get(this.apiUrl3);
-    response.subscribe((res) => console.log(res));
+
+    this.apiUrl3 = 'http://localhost:8080/api/login?email=' + email + '&password=' + wachtwoord;
+
+    const xhr = new XMLHttpRequest();
+    xhr.timeout = 2000;
+    xhr.onreadystatechange = function (e) {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          if (xhr.responseText === '[{"credentials": "true"}]') {
+            alert('Login sucses');
+          } else {
+            alert('Login info fout');
+          }
+        } else {
+          alert('No response from login server');
+        }
+      }
+    };
+    xhr.ontimeout = function () {
+      alert('somethiong went wrong');
+    };
+    xhr.open('get', this.apiUrl3, true);
+    xhr.send();
   }
 
 }
