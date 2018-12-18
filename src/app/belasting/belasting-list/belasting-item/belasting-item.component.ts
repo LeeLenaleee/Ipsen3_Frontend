a9c3f22b8f09d6f6a5769561c70873e7a55a3424
Wietse <wietsenicolaas@gmail.com>
2018-12-18 10:27:00 +0100

    Basis zoekfunctionaliteit in de tabel geimplementeerd. Begin gemaakt aan "Heeft betaald" toggle switch.

--- src/app/belasting/belasting-list/belasting-item/belasting-item.component.ts
+++ src/app/belasting/belasting-list/belasting-item/belasting-item.component.ts
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BelastingItemComponent implements OnInit {
 
+  input = ' ';
+  i: number;
+
+  // TODO: databaseContacten moet uiteindelijk uit de database gehaald worden en in een array komen.
+  databaseContacten = ['Simon', 'Jacco', 'Kasper', 'Jazzlyn', 'Wietse'];
+
+  contactList = []; // Een lijst voor de zoekfunctie.
+
+  textValue = '';
   constructor() { }
 
   ngOnInit() {
+  }
+
+  zoekContacten(event: any) {
+    this.input = (<HTMLInputElement>event.target).value.toUpperCase();
+    // @ts-ignore
+    this.contactList = document.getElementById('contactlijst').getElementsByTagName('td');
+
+    for (this.i = 0; this.i < this.contactList.length; this.i++) { // i is hierin de Index van de contactList, startend bij 0.
+      // @ts-ignore
+      if ((this.i % 3) === 1 && this.i < this.contactList.length) {
+        this.textValue = this.contactList[this.i].textContent;
+        if (this.textValue.toUpperCase().indexOf(this.input) > -1) { // Als de string die wordt ingevoerd voorkomt in het woord...
+          this.contactList[this.i - 1].style.display = ''; // Laat de rij dan zien.
+          this.contactList[this.i].style.display = '';
+          this.contactList[this.i + 1].style.display = '';
+        } else {
+          this.contactList[this.i - 1].style.display = 'none'; // Zo niet, laat de rij dan niet zien.
+          this.contactList[this.i].style.display = 'none';
+          this.contactList[this.i + 1].style.display = 'none';
+        }
+      } else {
+          continue;
+      }
+    }
   }
 
 }
