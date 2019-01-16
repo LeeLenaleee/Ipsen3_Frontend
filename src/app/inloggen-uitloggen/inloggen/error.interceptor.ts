def0b561dde202b515783cca006838dd6b203077
Jacco van den Berg <jaccoberg2281@gmail.com>
2019-01-16 14:20:43 +0100

    als je nu in een child route zit word je ook gewoon naar login pagina gelijdt, de return url is ook gone want niet netjes en security risk

--- src/app/inloggen-uitloggen/inloggen/error.interceptor.ts
+++ src/app/inloggen-uitloggen/inloggen/error.interceptor.ts
@@ -3,10 +3,12 @@ import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/c
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import {AuthenticationService} from './authentication.service';
+import {Router} from '@angular/router';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-  constructor(private authenticationService: AuthenticationService) {}
+  constructor(private authenticationService: AuthenticationService,
+              private router: Router) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(catchError(err => {
@@ -14,7 +16,7 @@ export class ErrorInterceptor implements HttpInterceptor {
         // auto logout if 401 response returned from api
         this.authenticationService.logout();
         // mooier I guess als error blijft
-        // setTimeout( () => {location.reload(true); }, 5000);
+        if (this.router.url !== '/login') { setTimeout( () => {location.reload(true); }, 0); }
       }
 
       console.log(err);
