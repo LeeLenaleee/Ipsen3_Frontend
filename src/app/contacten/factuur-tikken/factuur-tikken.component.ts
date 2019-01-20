ab7b930239dee1f258a0c237857f38bc698efb47
Jacco van den Berg <jaccoberg2281@gmail.com>
2019-01-20 18:37:49 +0100

    kan nu zoeken op facturen, moet oplossing vinden voor downloaden want kan nu niet door authorization

--- src/app/contacten/factuur-tikken/factuur-tikken.component.ts
+++ src/app/contacten/factuur-tikken/factuur-tikken.component.ts
@@ -1,4 +1,5 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import { DatePipe } from '@angular/common';
 import 'rxjs-compat/add/operator/map';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {FactuurModel} from '../../models/factuur.model';
@@ -17,6 +18,7 @@ export class FactuurTikkenComponent implements OnInit {
   @ViewChild('btwPercentage') btwPercentage: ElementRef;
   @ViewChild('btwKosten') btwKosten: ElementRef;
   @ViewChild('nettoKosten') nettoKosten: ElementRef;
+  @ViewChild('id') id: ElementRef;
   headers_object = new HttpHeaders({ 'Authorization': 'basic ' + btoa('test@test.com:9F86D081884C7D659A2FEAA0C55AD015A3BF4F1B' +
       '2B0B822CD15D6C15B0F00A08')});
   httpOptions = {
@@ -29,9 +31,10 @@ export class FactuurTikkenComponent implements OnInit {
   }
 
   zoek() {
-    this.http.get<FactuurModel>('http://localhost:8080/api/factuur/13', this.httpOptions).subscribe(
+    this.http.get<FactuurModel>('http://localhost:8080/api/factuur/' + this.id.nativeElement.value, this.httpOptions).subscribe(
       (factuur: FactuurModel) => {
-        this.datum.nativeElement.value = factuur.datum; this.aflvrDatum.nativeElement.value = factuur.afleverDatum;
+          this.datum.nativeElement.value = this.fromServerDateTransForm(factuur.datum);
+          this.aflvrDatum.nativeElement.value = this.fromServerDateTransForm(factuur.afleverDatum);
           this.omschrijving.nativeElement.value = factuur.factuurOmschrijving;
           this.brutoKosten.nativeElement.value = factuur.brutoKosten; this.btwPercentage.nativeElement.value = factuur.btwPercentage;
           this.btwKosten.nativeElement.value = factuur.btwKosten; this.nettoKosten.nativeElement.value = factuur.nettoKosten;
@@ -41,7 +44,8 @@ export class FactuurTikkenComponent implements OnInit {
   }
 
   voegToe() {
-    const factuurModel = new FactuurModel(this.datum.nativeElement.value, this.aflvrDatum.nativeElement.value,
+    const factuurModel = new FactuurModel(this.toServerDateTransform(this.datum.nativeElement.value),
+      this.toServerDateTransform(this.aflvrDatum.nativeElement.value),
       this.omschrijving.nativeElement.value, this.brutoKosten.nativeElement.value, this.btwPercentage.nativeElement.value,
       this.btwKosten.nativeElement.value, this.nettoKosten.nativeElement.value);
     console.log(factuurModel.nettoKosten);
@@ -69,7 +73,25 @@ export class FactuurTikkenComponent implements OnInit {
 
   download() {
     // niewe window openen zodat de download triggerd
-    window.open('http://localhost:8080/api/factuur/download?id=' + this.factNummer.nativeElement.value +
-      '&naam=' + 'FactuurNummer: ' + this.factNummer.nativeElement.value);
+    /*window.open('http://localhost:8080/api/factuur/download?id=' + this.factNummer.nativeElement.value +
+      '&naam=' + 'FactuurNummer: ' + this.factNummer.nativeElement.value);*/
+    const x = this.http.get<FactuurModel>('http://localhost:8080/api/factuur/download?id=' + this.factNummer.nativeElement.value +
+      '&naam=' + 'FactuurNummer: ' + this.factNummer.nativeElement.value, this.httpOptions);
+    const url = window.URL.createObjectURL(x);
+    window.open(url);
+    // if (!pwa || pwa.closed || typeof pwa.closed === 'undefined') {
+    //   alert( 'Please disable your Pop-up blocker and try again.');
+    // }
+  }
+
+  toServerDateTransform(date) {
+    const dateSendingToServer = new DatePipe('en-US').transform(date, 'dd-MM-yyyy');
+    return dateSendingToServer;
+  }
+
+  fromServerDateTransForm(date) {
+    const parts = date.split('-');
+    const x = parts[2] + '-' + parts[1] + '-' + parts[0];
+    return x;
   }
 }
